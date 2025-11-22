31b16fd7dd85a7a9cf2c1285953ed9f42b3e5db1
Chris January <chris@atomice.net>
2025-11-22 12:38:25 +0000

    Move nextp8 to machines/nextp8

--- scripts/make-sdcard.sh
+++ scripts/make-sdcard.sh
@@ -4,9 +4,10 @@ mkfs.vfat sdcard.img
 mkdir -p /tmp/sdcard
 sudo mount -o loop,uid=${LOGNAME} sdcard.img /tmp/sdcard
 oldpwd=${PWD}
-mkdir -p /tmp/sdcard/nextp8/carts
-cp ../femto8-nextp8/carts/*.p8 /tmp/sdcard/nextp8/carts/
-cp ../nextp8-loader/build/loader.bin /tmp/sdcard/nextp8/
-cp ../femto8-nextp8/build-nextp8/femto8.bin /tmp/sdcard/nextp8/nextp8.bin
+mkdir -p /tmp/sdcard/machines/nextp8/carts
+cp ../femto8-nextp8/carts/*.p8 /tmp/sdcard/machines/nextp8/carts/
+cp ../nextp8-loader/build/loader.bin /tmp/sdcard/machines/nextp8/
+cp ../femto8-nextp8/build-nextp8/femto8.bin /tmp/sdcard/machines/nextp8/nextp8.bin
+cp -r ../nextp8/machines/nextp8/core.cfg /tmp/sdcard/machines/nextp8/
 cd ${oldpwd}
 sudo umount /tmp/sdcard
